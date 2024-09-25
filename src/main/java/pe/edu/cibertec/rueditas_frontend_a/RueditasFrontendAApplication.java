c6f78978de6691d594bcb96f4ea9f6379bb98c96
User <mariajosegaspar95@gmail.com>
2024-09-24 23:44:02 -0500

    Carga inicial proyecto

---
+++ src/main/java/pe/edu/cibertec/rueditas_frontend_a/RueditasFrontendAApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_frontend_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasFrontendAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasFrontendAApplication.class, args);
+	}
+
+}
